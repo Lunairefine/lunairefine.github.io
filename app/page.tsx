94f7d22738a70cb8eabb73a48aaf6b3950005094
Lunairefine <teamcyber2809@gmail.com>
2025-11-16 08:50:41 +0700

    redo zzz zzz

--- app/page.tsx
+++ app/page.tsx
@@ -5,17 +5,6 @@ import Landing from './components/home';
 import About from './components/about';
 import Deployed from './components/deployed';
 
-export const metadata = {
-  other: {
-    "fc:frame": "vNext",
-    "fc:frame:image": "https://yourdomain.com/frame-0.png",
-    "fc:frame:button:1": "Mulai",
-    "fc:frame:button:1:action": "post",
-    "fc:frame:post_url": "https://yourdomain.com/api/frame"
-  }
-};
-
-
 export default function Home(): JSX.Element {
   return (
     <div>
