8e03851ffea2de3595fbc768281be7bdafad6f29
Lunairefine <teamcyber2809@gmail.com>
2025-12-20 08:15:42 +0700

    update metadata

--- app/layout.tsx
+++ app/layout.tsx
@@ -40,6 +40,20 @@ export const metadata: Metadata = {
     images: ["/media/images/frame.png"],
   },
   other: {
+    "fc:frame": JSON.stringify({
+    version: "next",
+    imageUrl: "https://lunairefine.vercel.app/media/images/frame.png",
+    button: {
+      title: "Explore my world",
+      action: {
+        type: "launch_frame",
+        name: "Syntax Lunaire",
+        url: "https://lunairefine.vercel.app",
+        splashImageUrl: "https://lunairefine.vercel.app/media/images/syntax.png",
+        splashBackgroundColor: "#000000",
+      },
+    },
+  }),
     'base:app_id': '6945e97bd77c069a945be1ec',
   },
 };
